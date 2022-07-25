e62417662ed0ce2a0c1bb3ad0a096fba4937eff2
Kenzie-XD <109863919+Kenzie-XD@users.noreply.github.com>
2022-07-25 17:13:04 +0700

    Update marshal.py

--- marshal.py
+++ marshal.py
@@ -1,13 +1,15 @@
+# For Python3
+
 # module
 import os
 import marshal
 
 os.system("clear")
-py = input("File > ")
+py = input("Input File > ")
 baca = open(py, "r").read()
 com = compile(baca, "", "exec")
 encrypt = marshal.dumps(com)
 baru = open("enc"+str("py"), "w")
 baru.write("import marrshal\n")
 baru.write("exec(marshal_loads("+repr(encrypt)+"))")
-print("Succes Encrypt | file name save as_ "+str(py))
+print("Succes Encrypt | file name save as "+str(py))
